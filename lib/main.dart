18a0f4687c2568a53f8705a4324e8a362e9f5ace
DylanLuna <galibander@hotmail.com>
2023-03-23 18:08:24 +0100

    Font Changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'GoogleSans',
       ),
       home: Home(),
     );
