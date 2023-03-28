ea6ad5804294ff90e2ffdf9565881ca89f39efd3
DylanLuna <galibander@hotmail.com>
2023-03-28 21:17:53 +0200

    Scrolling and Card

--- lib/accueil.dart
+++ lib/accueil.dart
@@ -25,6 +25,123 @@ class _AccueilState extends State<Accueil> {
     _futureGames = fetchAllGamesDetails();
   }
 
+  Widget createTopCard(){
+    return  Card(
+      child: Image.asset(
+        'assets/images/trex.jpg',
+        fit: BoxFit.cover,
+        height: 100,
+      ),
+    );
+  }
+  Widget createListView(BuildContext context, AsyncSnapshot snapshot){
+   return FutureBuilder<List<GamesDetails>>(
+      future: _futureGames,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<GamesDetails> gamesDetails = snapshot.data!;
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: gamesDetails.length < 15 ? gamesDetails.length : 15,
+            itemBuilder: (context, index) {
+              final GamesDetails gameDetails = gamesDetails[index];
+              return Card(
+                margin: const EdgeInsets.all(5),
+                color: c3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 100.0,
+                          height: 100.0,
+                          child: Image.network(gameDetails.headerImage),
+                        ),
+                        Container(
+                          width: 190.0,
+                          height: 100.0,
+                          padding: const EdgeInsets.only(
+                              left: 5, bottom: 5, top: 10),
+                          child: Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                gameDetails.name,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                gameDetails.publisher,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                gameDetails.price,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          width: 90.0,
+                          height: 100.0,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.circular(0.0),
+                              ),
+                              backgroundColor: c2,
+                              elevation: 0,
+                              padding: const EdgeInsets.all(2),
+                              textStyle: const TextStyle(fontSize: 18),
+                            ),
+                            child: const Text(
+                              'En savoir plus',
+                              textAlign: TextAlign.center,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Jeu()),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        return Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                CircularProgressIndicator(),
+                Text("Veuillez patienter"),
+              ]),
+        );
+      },
+   );
+  }
+
   Future<List<GamesDetails>> fetchAllGamesDetails() async {
     final List<int> appids = await fetchAllGames();
     final List<GamesDetails> gamesDetails = await fetchInfos(appids);
@@ -64,122 +181,18 @@ class _AccueilState extends State<Accueil> {
             ),
           ],
         ),
-        body: Column(children: [
-          Card(
-            child: Image.asset(
-              'assets/images/trex.jpg',
-              fit: BoxFit.cover,
-              height: 100,
-            ),
-          ),
-          FutureBuilder<List<GamesDetails>>(
-            future: _futureGames,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final List<GamesDetails> gamesDetails = snapshot.data!;
-                return Column(children: [
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount:
-                        gamesDetails.length < 15 ? gamesDetails.length : 15,
-                    itemBuilder: (context, index) {
-                      final GamesDetails gameDetails = gamesDetails[index];
-                      return Card(
-                        margin: const EdgeInsets.all(5),
-                        color: c3,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 100.0,
-                                  height: 100.0,
-                                  child: Image.network(gameDetails.headerImage),
-                                ),
-                                Container(
-                                  width: 190.0,
-                                  height: 100.0,
-                                  padding: const EdgeInsets.only(
-                                      left: 5, bottom: 5, top: 10),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        gameDetails.name,
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      Text(
-                                        gameDetails.publisher,
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      Text(
-                                        gameDetails.price,
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                const Spacer(),
-                                SizedBox(
-                                  width: 90.0,
-                                  height: 100.0,
-                                  child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(0.0),
-                                      ),
-                                      backgroundColor: c2,
-                                      elevation: 0,
-                                      padding: const EdgeInsets.all(2),
-                                      textStyle: const TextStyle(fontSize: 18),
-                                    ),
-                                    child: const Text(
-                                      'En savoir plus',
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => Jeu()),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  )
-                ]);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      CircularProgressIndicator(),
-                      Text("Veuillez patienter"),
-                    ]),
-              );
-            },
-          ),
-        ]));
+        body: Column(
+          children: [
+            createTopCard(),
+            Expanded(child:
+            FutureBuilder<List<GamesDetails>>(
+                future: _futureGames,
+                builder: (context, snapshot) {
+                    return createListView(context, snapshot);
+                }
+                )
+          )
+          ],
+        ));
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -118,7 +118,6 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: TextField(
                 controller: _emailController,
